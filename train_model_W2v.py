369abf84137b8163fb54e599cecbf69a281eac85
Amosse Edouard <eamosse@gmail.com>
2017-01-18 13:36:49 +0100

    Use different word embeddings vector for training and testig

--- train_model_W2v.py
+++ train_model_W2v.py
@@ -154,7 +154,7 @@ if __name__ == "__main__":
     parser = OptionParser('''%prog -o ontology -t type -f force ''')
     parser.add_option('-o', '--ontology', dest='ontology', default="dbpedia")
     parser.add_option('-t', '--type', dest='type', default="generic")
-    parser.add_option('-f', '--force', dest='force', default=0)
+    parser.add_option('-f', '--force', dest='force', default=0, type=int)
     parser.add_option('-c', '--classifier', dest='classifier', default='poly')
     opts, args = parser.parse_args()
     trainW2v(opts)
